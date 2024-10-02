2c316205f7dc21f636508221f620da3a6faf0c62
riemsloh <olaf.lueg@gmail.com>
2024-10-02 17:06:43 +0200

    update

---
+++ OpenWeatherMap/Views/PersonalWeatherStation.swift
@@ -0,0 +1,18 @@
+//
+//  PersonalWeatherStation.swift
+//  OpenWeatherMap
+//
+//  Created by Olaf on 02.10.24.
+//
+
+import SwiftUI
+
+struct PersonalWeatherStation: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PersonalWeatherStation()
+}
