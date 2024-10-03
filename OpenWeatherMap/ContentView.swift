1d28f8b79e2ba64ca726520a9686d9c40e60b726
riemsloh <olaf.lueg@gmail.com>
2024-10-04 01:14:20 +0200

    up

--- OpenWeatherMap/ContentView.swift
+++ OpenWeatherMap/ContentView.swift
@@ -27,7 +27,10 @@ struct ContentView: View {
             //Text(wuStation)
             Button("load", systemImage: "gearshape.arrow.triangle.2.circlepath"){
                 Task{
-await fetchShortRangeForecast()
+                    //await fetchShortRangeForecast()
+                   // await fetchCurrent()
+                    await fetchFiveDay()
+                   
                 }
             }
             
@@ -64,7 +67,90 @@ await fetchShortRangeForecast()
     
     
    // https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=b416b34806cb6cf5ed4c04c1b30e4e1b
+    @MainActor
+    func fetchCurrent() async{
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?lat=52.183&lon=8.417&appid=b416b34806cb6cf5ed4c04c1b30e4e1b&units=metric&lang=de")!
+        #if DEBUG
+            print("Lade current")
+        #endif
+        URLSession.shared.dataTask(with: url) { json, response, error in
+            do{
+                let json = json
+                let decoder = JSONDecoder()
+                do{
+                    let decodedResponse = try decoder.decode(Current.self, from: json!)
+                    print(decodedResponse)
+                    Task {
+                        //await shortRange.update(shortRangeForecast: decodedResponse)
+                        #if DEBUG
+                          //  await shortRange.printShortRange()
+                        #endif
+                    }
+                }catch{
+                    #if DEBUG
+                     print("Konnte nicht geladen werden")
+                    #endif
+                }
+            }
+        }.resume()
+    }
     
+    @MainActor
+    func fetchFiveDay() async{
+        let url = URL(string: "https://api.openweathermap.org/data/2.5/forecast?lat=52.183&lon=8.417&appid=b416b34806cb6cf5ed4c04c1b30e4e1b&units=metric&lang=de")!
+        #if DEBUG
+            print("Lade current")
+        #endif
+        URLSession.shared.dataTask(with: url) { json, response, error in
+            do{
+                let json = json
+                let decoder = JSONDecoder()
+                print(Date(timeIntervalSince1970: 1727989200))
+                do{
+                    let decodedResponse = try decoder.decode(FiveDayForecast.self, from: json!)
+                    print(decodedResponse)
+                    Task {
+                        //await shortRange.update(shortRangeForecast: decodedResponse)
+                        #if DEBUG
+                          //  await shortRange.printShortRange()
+                        #endif
+                    }
+                }catch{
+                    #if DEBUG
+                     print("Konnte nicht geladen werden")
+                    #endif
+                }
+            }
+        }.resume()
+    }
+    
+
+    func testi(){
+        let json  = """
+                        {
+                            "rain":{
+                                        "1h": 0.3
+                                    }
+                        }
+                        """.data(using: .utf8)!
+        
+        let decoder = JSONDecoder()
+        do{
+            let decodedResponse = try decoder.decode(Current.self, from: json)
+            print(decodedResponse)
+            Task {
+                //await shortRange.update(shortRangeForecast: decodedResponse)
+                #if DEBUG
+                  //  await shortRange.printShortRange()
+                #endif
+            }
+        }catch{
+            #if DEBUG
+             print("Konnte nicht geladen werden")
+            #endif
+        }
+        
+    }
 }
 
 #Preview {
--- OpenWeatherMap/Models/APIOpenWeatherMap.swift
+++ OpenWeatherMap/Models/APIOpenWeatherMap.swift
@@ -32,3 +32,209 @@ struct Stations: Codable{
     var altitude: Double
     var rank: Int
 }
+
+
+/**
+ JSON format API response fields
+
+ - coord
+    - coord.lon Longitude of the location
+    - coord.lat Latitude of the location
+ - weather (more info Weather condition codes)
+    - weather.id Weather condition id
+    - weather.main Group of weather parameters (Rain, Snow, Clouds etc.)
+    - weather.description Weather condition within the group. Please find more here. You can get the output in your language. Learn more
+    - weather.icon Weather icon id
+ base Internal parameter
+ main
+ main.temp Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+ main.feels_like Temperature. This temperature parameter accounts for the human perception of weather. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+ main.pressure Atmospheric pressure on the sea level, hPa
+ main.humidity Humidity, %
+ main.temp_min Minimum temperature at the moment. This is minimal currently observed temperature (within large megalopolises and urban areas). Please find more info here. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+ main.temp_max Maximum temperature at the moment. This is maximal currently observed temperature (within large megalopolises and urban areas). Please find more info here. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit
+ main.sea_level Atmospheric pressure on the sea level, hPa
+ main.grnd_level Atmospheric pressure on the ground level, hPa
+ visibility Visibility, meter. The maximum value of the visibility is 10 km
+ wind
+ wind.speed Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour
+ wind.deg Wind direction, degrees (meteorological)
+ wind.gust Wind gust. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour
+ clouds
+ clouds.all Cloudiness, %
+ rain
+ 1h(where available)Precipitation, mm/h. Please note that only mm/h as units of measurement are available for this parameter
+ snow
+ 1h(where available) Precipitation, mm/h. Please note that only mm/h as units of measurement are available for this parameter
+ dt Time of data calculation, unix, UTC
+ sys
+ sys.type Internal parameter
+ sys.id Internal parameter
+ sys.message Internal parameter
+ sys.country Country code (GB, JP etc.)
+ sys.sunrise Sunrise time, unix, UTC
+ sys.sunset Sunset time, unix, UTC
+ timezone Shift in seconds from UTC
+ id City ID. Please note that built-in geocoder functionality has been deprecated. Learn more here
+ name City name. Please note that built-in geocoder functionality has been deprecated. Learn more here
+ cod Internal parameter
+
+ */
+struct Current: Codable{
+    var main: Main
+    var rain: Rain?
+    var snow: Snow?
+    var wind: Wind
+    var weather: [Weather]
+    
+}
+
+/**
+ "weather": [
+      {
+         "id": 501,
+         "main": "Rain",
+         "description": "moderate rain",
+         "icon": "10d"
+      }
+   ],
+ */
+struct Weather: Codable{
+    var id: Int
+    var main: String
+    var description: String
+    var icon: String
+}
+/**
+ "main": {
+       "temp": 284.2,
+       "feels_like": 282.93,
+       "temp_min": 283.06,
+       "temp_max": 286.82,
+       "pressure": 1021,
+       "humidity": 60,
+       "sea_level": 1021,
+       "grnd_level": 910
+    },
+ */
+struct Main: Codable
+{
+    var temp: Double
+    var feels_like: Double
+    var temp_min: Double
+    var temp_max: Double
+    var pressure: Int
+    var humidity: Int
+    var sea_level: Int
+    var grnd_level: Int
+}
+
+struct Wind: Codable{
+    var speed: Double
+    var deg: Int
+    var gust: Double
+}
+
+
+struct Rain: Codable{
+    var houre: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case houre = "1h" // You can use "date" instead of "20170101".
+    }
+    
+    func encode(to encoder: Encoder) throws {
+       var container = encoder.container(keyedBy: CodingKeys.self)
+       try container.encode(houre, forKey: .houre)
+
+     }
+    
+    init(houre: Double) {
+      self.houre = houre
+
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        houre = try container.decode(Double.self, forKey: .houre)
+
+      }
+}
+
+struct Snow: Codable{
+    var houre: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case houre = "1h" // You can use "date" instead of "20170101".
+    }
+    
+    func encode(to encoder: Encoder) throws {
+       var container = encoder.container(keyedBy: CodingKeys.self)
+       try container.encode(houre, forKey: .houre)
+
+     }
+    
+    init(houre: Double) {
+      self.houre = houre
+
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        houre = try container.decode(Double.self, forKey: .houre)
+
+      }
+}
+struct Sys: Codable{
+    var type: Int
+    var id: Int
+    var country: String
+    var sunrise: Int
+    var sunset: Int
+    
+}
+
+
+
+
+struct FiveDayForecast: Codable{
+    var list: [ListForecast]
+    var city: City
+}
+
+struct ListForecast: Codable{
+    var dt: Int
+    var main: Main
+    var weather: [Weather]
+    var rain: Rain3h?
+    var pop: Double
+}
+
+struct Rain3h: Codable{
+    var houre: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case houre = "3h" // You can use "date" instead of "20170101".
+    }
+    
+    func encode(to encoder: Encoder) throws {
+       var container = encoder.container(keyedBy: CodingKeys.self)
+       try container.encode(houre, forKey: .houre)
+
+     }
+    
+    init(houre: Double) {
+      self.houre = houre
+
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        houre = try container.decode(Double.self, forKey: .houre)
+
+      }
+}
+
+struct City: Codable{
+    var name: String
+}
